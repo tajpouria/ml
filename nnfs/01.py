4aa9c04d25858cfa4415c6215cd79d7996ec9552
tajpouria <tajpouria.dev@gmail.com>
2023-06-17 23:43:17 +0200

    update 01.py

--- nnfs/01.py
+++ nnfs/01.py
@@ -1,11 +1,17 @@
 # inputs = [[input, weight]]
-inputs = [[1.3, 3.3], [3.5, 2.5], [5.1, 1.1], [2.4, 2.4]]
-bias = 3
+inputs = [1, 2, 3, 2.5]
+weights = [[0.2, 0.8, -0.5, 1.0], [0.5, -0.91, 0.26, -0.5], [0.7, 0.2, 0.1, 0.9]]
+bias = [2, 3, 0.5]
 
-output = bias
-for i in inputs:
-    output += i[0] * i[1]
 
-print("Inputs: " + str(inputs))
-print("Bias: " + str(bias))
-print("Output: " + str(output))
+outputs = []
+
+for i in range(len(weights)):
+    neuron_weights = weights[i]
+    neuron_output = bias[i]
+    for w in neuron_weights:
+        neuron_output += inputs[i] * w
+
+    outputs.append(neuron_output)
+
+print(outputs)
